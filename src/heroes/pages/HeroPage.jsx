0fd314dd6f144d941fd323d881577d8baf72c333
Orangel Jose Gonzalez <goranjose16@gmail.com>
2023-12-27 16:19:41 -0400

    Update index.html

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -22,7 +22,7 @@ export const HeroPage = () => {
       <div className="col-4">
         <img
           className="img-thumbnail animate__animated animate__fadeInLeft"
-          src={`/assets/heroes/${id}.jpg`}
+          src={`/heroes/${id}.jpg`}
           alt={hero.superhero}
         />
       </div>
