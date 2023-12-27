70176cf46d00c1c7c916c5db57d0c37e63f167cc
Orangel Jose Gonzalez <goranjose16@gmail.com>
2023-12-27 15:44:17 -0400

    First commit

---
+++ src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,11 @@
+import { HeroList } from '../components'
+
+export const MarvelPage = () => {
+  return (
+    <div>
+      <h1>Marvel Comics</h1>
+      <hr />
+      <HeroList publisher='Marvel Comics' />
+    </div>
+  )
+}
